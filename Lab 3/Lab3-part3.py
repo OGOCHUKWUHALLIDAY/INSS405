c3c8bb391a3f3bdd3b70a49458370744a039ab2e
OGOCHUKWUHALLIDAY <ogochukwu.halliday@gmail.com>
2023-03-08 19:40:57 -0500

    Lab 3

---
+++ Lab 3/Lab3-part3.py
@@ -0,0 +1,7 @@
+num = input( 'Enter input num')
+print(num)
+
+if int(num)% 2 == 0:
+    print('even')
+else:
+    print('odd')
---
+++ lab.py
@@ -0,0 +1,88 @@
+print("Hi everyone !.")
+print("I wish to be in school today.")
+print("who knows if tomorrow is holiday?")
+print("I literly gonna miss you.")
+print("My son was coming home yesterday for his birthday celebration")
+print("I wish I could travel to Nigeria next year.")
+print("Who else is coming with me?.")
+print("The python lab class was interesting,I hope you enjoyed it too?.")
+print("Does anyone knows how to bake coconut bread?")
+if 5 > 2:
+    print("Five is greater than two!")
+    #This is my home
+    print("Hello,world!")
+    x = 5
+    y = "john"
+    print(x)
+    print(y)
+    myvar = "John"
+print(myvar)
+my_var = "John"
+print(my_var)
+_my_var = "John"
+print(_my_var)
+myVar = "John"
+print(myVar)
+MYVAR = "John"
+print(MYVAR)
+myvar2 = "John"
+print(myvar2)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x,y,z = "Peter", "Paul", "Angela"
+print(y)
+print(x)
+print(z)
+x = "Angla"
+y = "is"
+z = "print"
+print(x,y,z)
+x = "Please"
+y = "come"
+z = "home"
+print(x,y,z)
+x = "awesome"
+print("Python is " + x)
+x = "love"
+print("Ama is " + x)
+x = 5
+print(int,(x))
+y = 9
+print(int,(9))
+x = "Hello World"
+print(str,("Hello World"))
+y = 10
+print(int,(10))
+x = ("apple", "banana", "cherry")
+print(tuple,("apple", "banana", "cherry"))
+x = ("apple", "banana", "cherry")
+print(set,("apple", "banana", "cherry"))
+x = 1
+y = 2.8
+z = 1j
+print(int(x))
+print(float(y))
+print(complex(z))
+x = b"Hello"
+print(bytes(b"Hello"))
+x = bytearray(5)
+print(bytearry(5))
+x = 1.10
+y = 1.0
+z = -35.59
+print(int(1.10))
+print(floa(1.0))
+print(complex(-35.59))
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(int(1))
+print(int(2.8))
+print(int("3"))
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
