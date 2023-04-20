2e1b7f1caf70b20e57fc0f7e1b29d391bfc62930
OGOCHUKWUHALLIDAY <ogochukwu.halliday@gmail.com>
2023-04-19 21:40:44 -0400

    Assignment 1

--- Assignment1/Q3.py
+++ Assignment1/Q3.py
@@ -1,26 +1,28 @@
-num_courses = 18 # choose 18 as number of courses taken by the user
-total_score = 0
 
-#create a loop of 18 numbers and calculate a total score
-for i in range(18):
-    score = int(input(f"Enter final score for course {i+1}: "))
-    total_score += score
+#choose 15 as the number of courses taken by the user
 
+subjects = 15
+sum= 0.00
 
-average_score = total_score / num_courses
-# To determine the grades: use #If and elf conditions:
+#create a loop of 15 numbers and calculate the total score:
 
-if average_score > 90:
-    grade = "A"
-elif average_score > 80:
-    grade = "B"
-elif average_score >= 75:
-    grade = "C"
-elif average_score > 60:
-    grade = "D"
+for i in range(15):
+    score = input("Enter final score for course")
+    sum=sum+int(score)
+
+#To perform the average
+average = sum / 15
+print("Total sum=", sum)
+print("Average", average)
+
+#To determine the grades use the if and elif statement:
+if(int(average) > 90):
+    print("A")
+elif(int(average) > 80):
+    print("B")
+elif(int(average) >= 75 and int(average) <= 79):
+    print("C")
+elif(int(average) > 60) and int(score)<=74:
+    print("D")
 else:
-    grade = "F"
-#To carryout conditional statements:
-print(f"Total score: {total_score}")
-print(f"Average score: {average_score}")
-print(f"Grade:{grade}")
+    print("F")
